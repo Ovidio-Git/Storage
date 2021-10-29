7cd84aa4bc60c57d63e8b33f43b6ebc63581a591
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-10-28 20:35:55 -0500

    Create Insert_in_table_lybrary.sql

---
+++ Mysql/Insert_in_table_lybrary.sql
@@ -0,0 +1,28 @@
+
+
+INSERT INTO authors(author_id, name, nationality)
+VALUES(null,'Juan Rulfo','MEX');
+
+INSERT INTO authors(name, nationality)
+VALUES('Gabriel Gracia Marquez', 'COL');
+
+INSERT INTO authors()
+VALUES(null, 'Juan Grabriel Vasquez', 'COL');
+
+INSERT INTO authors(name, nationality)
+VALUES
+('Julio Cortazar', 'ARG'), 
+('Isabel Allende','CHI'),
+('Octavio Paz', 'MEX'),
+('Juan Carlos Onetti', 'URU'),
+('Pablo Neruda','CHI'),
+('Mario Vargas LLosa', 'PER'),
+('Mario Benedetti', 'URU'),
+('Paulo Coelho', 'BRA');
+
+INSERT INTO `clients`(client_id, name, email, birthdate, gender, active, created_at, updated_at)
+VALUES 
+('Maria Dolores Gomez','Maria Dolores.95983222J@random.names','1971-06-06','F',1,'2018-04-09 16:51:30'),
+('Adrian Fernandez','Adrian.55818851J@random.names','1970-04-09','M',1,'2018-04-09 16:51:30'),
+('Maria Luisa Marin','Maria Luisa.83726282A@random.names','1957-07-30','F',1,'2018-04-09 16:51:30'),
+('Pedro Sanchez','Pedro.78522059J@random.names','1992-01-31','M',1,'2018-04-09 16:51:30');

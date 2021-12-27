33e40b9cb693aae629e5708309fae8d3ae030edc
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-12-27 01:26:16 -0500

    Create Makelist.py

---
+++ EA/Makelist.py
@@ -0,0 +1,8 @@
+def run():
+  data = input("Inserte la data para Splitear:\n")
+  print("--------- Data procesada y spliteada ---------")
+  print(data.split("|"))
+
+
+if __name__=='__main__':
+  run()

9e45257b7973881f2669cde43942a56277098ab7
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-12-27 13:10:19 -0500

    Update Create.js

--- EA/Create.js
+++ EA/Create.js
@@ -7,21 +7,26 @@
  * Date: 26/12/2021
  */
 const memory =['Aceptación del servicio', ' Acreditación Cuenta Individual (BEPS)', ' Acta de notificación', ' Acta liquidación', ' Actividades de divulgación', ' Actividades de vinculación', ' Acto Administrativo', ' Acto Administrativo de Pago de Incapacidades', ' Acto Administrativo de Reconocimiento', ' Acuerdo de Pago', ' Afiliación', ' Afiliación (Colombianos en el exterior)', ' Afiliación (Subsidiada)', ' Afiliación (por Sentencia/ Tutela)', ' Anulación de Afiliación', ' Auditoria Reconocimiento', ' Autoliquidación', ' Autorización de procesamiento automático',' Auxilio Funerario']
+
+
 function main()
 {  
-   Repository.EnsureOutputVisible( "Script" );
-   Repository.ClearOutput("Script");
-   var pkgColpensiones as EA.Package; 
+	Repository.EnsureOutputVisible( "Script" );
+	Repository.ClearOutput("Script");
+	var pkgColpensiones as EA.Package; 
 
-   pkgColpensiones = Repository.GetTreeSelectedPackage();   
-   pkgElements = pkgColpensiones.Elements;
-	for (var i=0; i<=memory.length;i++){
-	   pkgElements.AddNew(memory[i], 'ArchiMate_DataObject');
+	pkgColpensiones = Repository.GetTreeSelectedPackage();   
+	pkgElements = pkgColpensiones.Elements;
+
+	for (var i=0; i<memory.length;i++){
+	  pkgElements.AddNew(memory[i], 'ArchiMate_DataObject');
 	}
- 
-   Session.Output("Values0: " + pkgColpensiones.Name);
-   Session.Output("Values1: " + pkgElements.Name);
-   pkgColpensiones.Update()	
+
+	Session.Output("Pkg Workspace: " + pkgColpensiones.Name);
+	Session.Output("New Elements: " + memory.length);
+	pkgColpensiones.Update()	
+	
+
 }
 
 main();

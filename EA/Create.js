952568b11364e82a024fba4fb006f8f43a8cda43
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-12-28 00:53:36 -0500

    Update Create.js

--- EA/Create.js
+++ EA/Create.js
@@ -1,32 +1,42 @@
 !INC Local Scripts.EAConstants-JScript
-
 /*
  * Script Name: Create diagram
  * Author: Ovidio Andrade
  * Purpose: 
  * Date: 26/12/2021
  */
-const memory =['Aceptación del servicio', ' Acreditación Cuenta Individual (BEPS)', ' Acta de notificación', ' Acta liquidación', ' Actividades de divulgación', ' Actividades de vinculación', ' Acto Administrativo', ' Acto Administrativo de Pago de Incapacidades', ' Acto Administrativo de Reconocimiento', ' Acuerdo de Pago', ' Afiliación', ' Afiliación (Colombianos en el exterior)', ' Afiliación (Subsidiada)', ' Afiliación (por Sentencia/ Tutela)', ' Anulación de Afiliación', ' Auditoria Reconocimiento', ' Autoliquidación', ' Autorización de procesamiento automático',' Auxilio Funerario']
-
+const memory =['Actividades de vinculación', 'Sustanciación','Tiempos Públicos', 'Tipos de PQRS', 'Trámite', 'Trámite (BEPS)', 'Trámite (RPM)']
 
 function main()
 {  
 	Repository.EnsureOutputVisible( "Script" );
 	Repository.ClearOutput("Script");
-	var pkgColpensiones as EA.Package; 
-
-	pkgColpensiones = Repository.GetTreeSelectedPackage();   
-	pkgElements = pkgColpensiones.Elements;
-
-	for (var i=0; i<memory.length;i++){
-	  pkgElements.AddNew(memory[i], 'ArchiMate_DataObject');
-	}
-
-	Session.Output("Pkg Workspace: " + pkgColpensiones.Name);
-	Session.Output("New Elements: " + memory.length);
-	pkgColpensiones.Update()	
 	
+	
+	var pkgColpensiones as EA.Package;
+	var dgmColpensiones as EA.Diagram;
+	var objDiagramas as EA.DiagramObject;
+
+	pkgColpensiones = Repository.GetTreeSelectedPackage(); 
+	dgmColpensiones = Repository.GetDiagramByID(5);
+	pkgElements     = pkgColpensiones.Elements;
+
+	
+	for (var i=0; i<memory.length;i++){
+		NewElements = pkgElements.AddNew(memory[i], 'ArchiMate_DataObject');
+		objDiagramas = dgmColpensiones.DiagramObjects.AddNew("l=200;r=400;t=200;b=270;","");
+		objDiagramas.ElementID = NewElements.ElementID
+		objDiagramas.Update()
+	}
+	
+	Session.Output("[WORKSPACE] Pakage:     " + pkgColpensiones.Name);
+	Session.Output("[WORKSPACE] Diagram:   " + dgmColpensiones.Name);
+	Session.Output("[PROCESS] Adding " + memory.length + " New Elements");	
+	if (NewElements.Update()){
+		Session.Output("[OK] Successful process")
+	}
+	
+	 
 
 }
-
 main();

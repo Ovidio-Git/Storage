5f2510de9ae7d55e7d406dd670451322fec907a5
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-12-29 16:41:05 -0500

    Update Create.js

--- EA/Create.js
+++ EA/Create.js
@@ -5,38 +5,36 @@
  * Purpose: 
  * Date: 26/12/2021
  */
-const memory =['Actividades de vinculación', 'Sustanciación','Tiempos Públicos', 'Tipos de PQRS', 'Trámite', 'Trámite (BEPS)', 'Trámite (RPM)']
+const CreateElement = ['ArchiMate_Artifact','ArchiMate_DataObject','ArchiMate_ApplicationComponent']
+const memory =[ 'EXCEL', 'CONSULTA PAGOS INCONSISTENCIAS', 'CORRECCIONES PAGOS (CORRDNC)', 'MODIDNC', 'HISTORICO SUBSIDIADOS', 'LIQUIDADOR', 'CONSGRE', 'CONSDNC', 'VERIGRE PAGOS', 'COMBINACION DE AUTOLIQUIDACIONES DE CORRECCIÓN', 'AUTOLISS', 'SAP - ERP', 'HISTORIA LABORAL MASIVA', 'MODICAN', 'HISTORIA LABORAL UNIFICADA', 'HPOO', 'VERIGRE', 'BIZAGI', 'MODISUBSI', 'CONSIDE']
 
 function main()
 {  
 	Repository.EnsureOutputVisible( "Script" );
 	Repository.ClearOutput("Script");
 	
-	
+	const ctrl = [false, true];
 	var pkgColpensiones as EA.Package;
 	var dgmColpensiones as EA.Diagram;
 	var objDiagramas as EA.DiagramObject;
 
 	pkgColpensiones = Repository.GetTreeSelectedPackage(); 
-	dgmColpensiones = Repository.GetDiagramByID(5);
+	dgmColpensiones = Repository.GetDiagramByGuid('{5E62CA76-13CA-4600-9BF6-0A50BE82A019}');
 	pkgElements     = pkgColpensiones.Elements;
 
-	
-	for (var i=0; i<memory.length;i++){
-		NewElements = pkgElements.AddNew(memory[i], 'ArchiMate_DataObject');
-		objDiagramas = dgmColpensiones.DiagramObjects.AddNew("l=200;r=400;t=200;b=270;","");
-		objDiagramas.ElementID = NewElements.ElementID
-		objDiagramas.Update()
+	if(ctrl[0]){
+		for (var i=0; i<memory.length;i++){
+			NewElements  = pkgElements.AddNew(memory[i], CreateElement[2]);
+			objDiagramas = dgmColpensiones.DiagramObjects.AddNew("l=200;r=400;t=200;b=270;","");
+			objDiagramas.ElementID = NewElements.ElementID
+			objDiagramas.Update()
+		}
 	}
-	
 	Session.Output("[WORKSPACE] Pakage:     " + pkgColpensiones.Name);
-	Session.Output("[WORKSPACE] Diagram:   " + dgmColpensiones.Name);
+	Session.Output("[WORKSPACE] Diagram:   "  + dgmColpensiones.Name);
 	Session.Output("[PROCESS] Adding " + memory.length + " New Elements");	
 	if (NewElements.Update()){
 		Session.Output("[OK] Successful process")
 	}
-	
-	 
-
 }
 main();

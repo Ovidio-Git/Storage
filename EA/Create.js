4669529556120e6d3097813a63addf6063a5aee3
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2022-01-19 11:58:07 -0500

    Update Create.js

--- EA/Create.js
+++ EA/Create.js
@@ -5,39 +5,49 @@
  * Purpose: 
  * Date: 26/12/2021
  */
-const ElementTypes    = ['ArchiMate_Artifact','ArchiMate_ApplicationComponent','ArchiMate_DataObject']
-const ElementMeasures = ['l=260;r=400;t=200;b=250;','l=260;r=400;t=200;b=270;']
-const ElementNames    = ['FD_RADICA']
+ 
+const power           = {"off":false, "on":true}
+const ElementStorage  = ['ArchiMate_Artifact','ArchiMate_DataObject'];
+const ElementTypes    = ['ArchiMate_BusinessFunction','ArchiMate_BusinessProcess','ArchiMate_ApplicationService', 'ArchiMate_ApplicationComponent','ArchiMate_BusinessActor','ArchiMate_Location'];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+const ElementMeasures = ['l=260;r=400;t=200;b=250;','l=260;r=400;t=200;b=270;'];
+const ctrlProscess    = power["off"];
+const targetDiagram   = '{92BA5D0F-F446-4611-AE38-9FFA3912D8F5}';                                                                                                                                                                                          
+const ElementType     = ElementTypes[4];
+const ElementNames    = ['Afiliado', 'Apoderado', 'Tercero', 'Empleador'];    
+
 
 
 function main()
 {  
 	Repository.EnsureOutputVisible( "Script" );
 	Repository.ClearOutput("Script");
-	
-	const ctrl = [false, true];
+
 	var pkgColpensiones as EA.Package;
 	var dgmColpensiones as EA.Diagram;
 	var objDiagramas as EA.DiagramObject;
 
 	pkgColpensiones = Repository.GetTreeSelectedPackage(); 
-	dgmColpensiones = Repository.GetDiagramByGuid('{ECA94A63-08B2-4379-AFD9-E1373247CAA3}');
+	dgmColpensiones = Repository.GetDiagramByGuid(targetDiagram);
 	pkgElements     = pkgColpensiones.Elements;
 	
 	Session.Output("[WORKSPACE] Pakage:     " + pkgColpensiones.Name);
 	Session.Output("[WORKSPACE] Diagram:   "  + dgmColpensiones.Name);
-		
-	if(ctrl[1]){
-		Session.Output("[PROCESS] Adding " + ElementNames.length + " New Elements");
+	
+	
+	if(ctrlProscess){
+		Session.Output("[PROCESS] Adding " + ElementNames.length + " New Elements type "+ ElementType);
 		for (var i=0; i<ElementNames.length;i++){
-			NewElements  = pkgElements.AddNew(ElementNames[i], ElementTypes[0]);
+			NewElements  = pkgElements.AddNew(ElementNames[i], ElementType);
 			objDiagramas = dgmColpensiones.DiagramObjects.AddNew(ElementMeasures[1],"");
-			objDiagramas.ElementID = NewElements.ElementID
-			objDiagramas.Update()
+			objDiagramas.ElementID = NewElements.ElementID;
+			objDiagramas.Update();
 		}
 		if (NewElements.Update()){
-			Session.Output("[OK] Successful process")
+			Session.Output("[OK] Successful process");
 		}
+	}
+	else {
+		Session.Output("[PROCESS] Status: OFF");
 	}
 }
 main();
